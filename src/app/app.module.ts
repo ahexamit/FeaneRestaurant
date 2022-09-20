f039e5be4339361db823db561b3c6858cd5849b6
Amit Kumar <amit.kumar@ahex.co.in>
2022-09-20 17:22:00 +0530

    menuPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    DashboardComponent
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,18 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../shared/service.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  public menuData: Array<any> = [];
+  public count: number = 6;
+  public localMenuData: Array<any> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sourceData: ServiceService) {
+    this.sourceData.getJson().subscribe((data) => {
+      this.localMenuData = data;
+      this.menuData = this.localMenuData.slice(0, 6);
+      console.log(data);
+    });
   }
-  showAll(){
-    
-  }
 
+  ngOnInit(): void {}
+
+  public currentCategory = 'all';
+  //Method to see category wise array items//
+  public showAll(event: string) {
+    this.currentCategory = event;
+    if (event == 'all') {
+      this.menuData = this.localMenuData.slice(0, 6);
+      this.count = 6;
+    } else {
+      console.log(event);
+      this.menuData = this.localMenuData
+        .filter((x) => {
+          return x.categories == event;
+        })
+        .slice(0, 4);
+    }
+  }
+  //Method to see more array items//
+  public seeMore() {
+    this.count = this.count + 3;
+    if (this.currentCategory == 'all') {
+      this.menuData = this.localMenuData.slice(0, this.count);
+      console.log(this.menuData);
+    } else {
+      this.menuData = this.localMenuData
+        .filter((x) => {
+          return x.categories == this.currentCategory;
+        })
+        .slice(0, this.count);
+    }
+  }
 }
