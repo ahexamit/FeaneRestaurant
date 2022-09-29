947334f1ea1487ba20ede468f9815afda07c83d0
amit kumar <amitraobabulal@gmail.com>
2022-09-29 17:18:35 +0530

    place order button &  book tab

--- src/app/booktable/booktable.component.ts
+++ src/app/booktable/booktable.component.ts
@@ -1,35 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-booktable',
   templateUrl: './booktable.component.html',
-  styleUrls: ['./booktable.component.css']
+  styleUrls: ['./booktable.component.css'],
 })
 export class BooktableComponent implements OnInit {
-public submitted:boolean= false;
-public bookTableForm!:FormGroup;
+  public submitted: boolean = false;
+  public bookTableForm!: FormGroup;
 
-  constructor(private fb:FormBuilder){}
-
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.bookForm();
   }
-  public bookForm():void{
-    this.bookTableForm=this.fb.group({
-      name:['',Validators.required],
-      email:['',[Validators.required,Validators.email]],
-        mobile:['',Validators.required],
-        time:['',Validators.required],
-        people:['',Validators.required]
-    })
+  public bookForm(): void {
+    this.bookTableForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', Validators.required],
+      time: ['', Validators.required],
+      people: ['', Validators.required],
+    });
   }
-  public get f():{[key:string]:AbstractControl}  {
+  public get f(): { [key: string]: AbstractControl } {
     return this.bookTableForm.controls;
-
   }
-  public submit():void{
+  public submit(): void {
     this.submitted = true;
   }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,22 +7,33 @@ import { CartService } from '../shared/cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  public product: any = [];
+  public product: Array<any> = [];
   public grandTotal!: number;
+  public placeOrder!: boolean;
 
   constructor(private cartservice: CartService) {}
 
   ngOnInit() {
     this.init();
+    this.placeOrderButton();
   }
-  public removeItem(item: any):void {
+  public removeItem(item: any): void {
     this.cartservice.removeCartItem(item);
   }
 
   private init(): void {
     this.cartservice.getProducts().subscribe((res) => {
       this.product = res;
+      console.log(this.product.length);
       this.grandTotal = this.cartservice.getTotalPrice();
     });
+  }
+
+  public placeOrderButton() {
+    if (this.product.length > 0) {
+      return (this.placeOrder = true);
+    } else {
+      return (this.placeOrder = false);
+    }
   }
 }
